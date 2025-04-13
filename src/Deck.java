5268069b2a7198831e86ea20d255dad5db06bbf4
Eddie Stump <ejstump13@gmail.com>
2025-04-13 16:53:21 -0400

    changed deck to arrayList

--- src/Deck.java
+++ src/Deck.java
@@ -1,12 +1,11 @@
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Represents a deck of cards and the logic surrounding the deck
  */
 public class Deck {
-    private final Card[] cards;
-    private static final int MAX_SIZE = 52;
-    private int currentSize;
+    private final ArrayList<Card> cards;
 
 
     public Deck(){
@@ -17,15 +16,14 @@ public class Deck {
         int[] values = {14, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
 
         // Create deck
-        cards = new Card[MAX_SIZE];
+        cards = new ArrayList<>();
 
         // Create cards in deck
         for(int i = 0; i < 4; i++) {
             for(int j = 0; j < 13; j++) {
-                cards[j + 13*i] = new Card(suits[i], ranks[j], values[j]);
+                cards.add(new Card(suits[i], ranks[j], values[j]));
             }
         }
-        currentSize = MAX_SIZE;
     }
 
 
@@ -34,24 +32,8 @@ public class Deck {
      * @return the dealt card
      */
     public Card deal() {
-        currentSize--;
-        Card temp = cards[currentSize];
-        cards[currentSize] = null;
-        return temp;
-    }
-
-
-    /**
-     * Return a card to the top of the deck
-     * @param c the card to return
-     * @return true if success, false otherwise
-     */
-    public boolean returnToDeck(Card c) {
-        if(currentSize < MAX_SIZE) {
-            cards[currentSize] = c;
-            currentSize++;
-            return true;
-        } else return false;
+        if (cards.size() == 0) return null;
+        return cards.remove(cards.size() - 1);
     }
 
 
@@ -60,12 +42,7 @@ public class Deck {
      * @param returned the cards to be returned
      */
     public void returnToDeck(List<Card> returned) {
-
-        for(Card c : returned) {
-            if(c != null) {
-                if(!returnToDeck(c)) return;
-            }
-        }
+        cards.addAll(returned);
     }
 
 
@@ -74,13 +51,15 @@ public class Deck {
      */
     public void shuffle() {
 
-        for(int i = 0; i < currentSize; i++) {
+        for(int i = 0; i < cards.size(); i++) {
 
-            int rPosition = (int)(Math.random()* currentSize);
+            // Choose location to swap
+            int rPosition = (int) (Math.random() * cards.size());
 
-            Card temp = cards[i];
-            cards[i] = cards[rPosition];
-            cards[rPosition] = temp;
+            // Swap cards
+            Card temp = cards.get(i);
+            cards.set(i, cards.get(rPosition));
+            cards.set(rPosition, temp);
         }
     }
 
@@ -129,70 +108,26 @@ public class Deck {
      */
     public boolean checkDeck() {
 
-        int spadesCnt=0, diamondsCnt=0, clubsCnt=0, heartsCnt=0;
-        int totalValue=0;
-        Card[] spades = new Card[20];
-        Card[] diamonds = new Card[20];
-        Card[] hearts = new Card[20];
-        Card[] clubs = new Card[20];
-        System.out.println("*******Checking Deck **********/");
-        for(int i=0; i<MAX_SIZE; i++) {
-            if(cards[i]!=null && cards[i].getSuit().equals("Clubs")) {
+        // Ensure deck is correct size
+        if (cards.size() != 52) return false;
 
-                clubs[clubsCnt]=cards[i];
-                clubsCnt++;
-            }
-            else if(cards[i]!=null && cards[i].getSuit().equals("Diamonds")) {
-                diamonds[diamondsCnt]=cards[i];
-                diamondsCnt++;
-            }
+        Deck temp = new Deck();
+        Card current = temp.deal();
 
-            else if(cards[i]!=null && cards[i].getSuit().equals("Hearts")) {
-                hearts[heartsCnt]=cards[i];
-                heartsCnt++;
-            }
+        // Check all cards in deck against a new deck
+        while (current != null) {
+            if (!cards.contains(current)) return false;
 
-            else if(cards[i]!=null && cards[i].getSuit().equals("Spades")) {
-                spades[spadesCnt]=cards[i];
-                spadesCnt++;
-            }
-            if(cards[i]!=null)
-                totalValue+=cards[i].getValue();
-
+            current = temp.deal();
         }
-        for(int i=0; i<clubsCnt; i++)
-            if(clubs[i]!=null)
-                System.out.println(clubs[i]);
 
-        System.out.println();
-
-        for(int i=0; i<diamondsCnt; i++)
-            if(diamonds[i]!=null)
-                System.out.println(diamonds[i]);
-
-        System.out.println();
-
-        for(int i=0; i<spadesCnt; i++)
-            if(spades[i]!=null)
-                System.out.println(spades[i]);
-
-        System.out.println();
-
-        for(int i=0; i<heartsCnt; i++)
-            if(hearts[i]!=null)
-                System.out.println(hearts[i]);
-
-        System.out.println("Clubs: " + clubsCnt + " Spades: " + spadesCnt +
-                " Diamonds: " + diamondsCnt + " Hearts: " + heartsCnt);
-        System.out.println("Total: " + totalValue);
-
-        return clubsCnt == 13 && spadesCnt == 13 && diamondsCnt == 13 && heartsCnt == 13 && totalValue == 416;
+        return true;
     }
 
 
     public String toString(){
 
-        StringBuilder out = new StringBuilder("Size: " + currentSize + "\n");
+        StringBuilder out = new StringBuilder("Size: " + cards.size() + "\n");
         for(Card c : cards){
             if(c != null){
                 out.append(c).append("\n");
